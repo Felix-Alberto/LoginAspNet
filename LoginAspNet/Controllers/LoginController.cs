9ba5b75fdbc90b88fb972f01707ae57c2abe58ba
Felix <felialberto.ti226@gmail.comx>
2023-07-05 00:21:10 -0300

    Add index

---
+++ LoginAspNet/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LoginAspNet.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ LoginAspNet/Models/LoginModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LoginAspNet.Models
+{
+    public class LoginModel
+    {
+
+        [Required(ErrorMessage = "Digite o login")]
+        public string? Login { get; set; }
+
+        [Required(ErrorMessage = "Digite a senha")]
+        public string? Password { get; set; }
+
+        public bool Remember_Me { get; set;}
+}
+}
