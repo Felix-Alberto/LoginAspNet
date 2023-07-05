643b38bb0e3f572b01294966c4117fa46e877647
Felix <felialberto.ti226@gmail.comx>
2023-07-04 23:24:54 -0300

    commit initial

---
+++ LoginAspNet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LoginAspNet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
